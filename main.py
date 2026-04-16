1d02b4d4d7588804ab165503283d4ae774e956eb
权权 <qzz205611@163.com>
2026-04-16 16:24:49 +0800

    今天重新写了一遍，添加了while功能

--- main.py
+++ main.py
@@ -1,74 +1,75 @@
 def parse_numbers():
     raw_text = input("请输入一串数字：")
 
+    if raw_text =='q':
+        return 'q'
+    
     if raw_text.strip() == "":
         print("请至少输入一个整数")
         return None
-
+    
     try:
-        parts = raw_text.split()
-        numbers = []
+            parts = raw_text.split()
+            numbers =[]
+            for text_num in parts:
+                numbers.append(int(text_num))
+            return numbers
+    
+    except ValueError:
+            print("请输入正确的整数数据")
+            return None
 
-        for text_num in parts:
-            numbers.append(int(text_num))
-
-        return numbers
-
-    except:
-        print("请输入正确的整数数据")
-        return None
-
-
-def analyze_numbers(numbers):
+        
+def analyzer_numbers(numbers):
+    total_sum = 0
     total_count = 0
     even_count = 0
     odd_count = 0
     big_count = 0
-    total_sum = 0
     big_sum = 0
-
+    average = 0
     for num in numbers:
-        total_count = total_count + 1
-        total_sum = total_sum + num
-
+        total_count += 1
+        total_sum += num
         if num % 2 == 0:
-            even_count = even_count + 1
-
+            even_count += 1
         if num % 2 != 0:
-            odd_count = odd_count + 1
-
+            odd_count += 1
         if num > 10:
-            big_count = big_count + 1
-            big_sum = big_sum + num
-
+            big_count += 1
+            big_sum += num
     average = total_sum / total_count
-    return {
-        "total_count": total_count,
-        "total_sum": total_sum,
-        "even_count": even_count,
-        "odd_count": odd_count,
-        "big_count": big_count,
-        "big_sum": big_sum,
-        "average": average
+    return{"total_sum":total_sum,
+           "total_count":total_count,
+           "even_count":even_count,
+           "odd_count":odd_count,
+           "big_count":big_count,
+           "big_sum":big_sum,
+           "average":average
     }
 
 def print_result(numbers, result):
-    print("原始数字列表：", numbers)
-    print("总数的个数：", result["total_count"])
-    print("总数的总和：", result["total_sum"])
-    print("偶数的个数：", result["even_count"])
-    print("奇数的个数：", result["odd_count"])
-    print("大于10的数的个数：", result["big_count"])
-    print("大于10的数的和：", result["big_sum"])
-    print("平均数：", result["average"])
+    print("总和为：",result["total_sum"])
+    print("总数个数为:",result["total_count"])
+    print("偶数个数为:",result["even_count"])
+    print("奇数个数为:",result["odd_count"])
+    print("大于10的总数个数:",result["big_count"])
+    print("大于10的个数的总数为:",result["big_sum"])
+    print("原始数字列表:",result["numbers"])
+    print("平均数为:"result["average"])
 
-def main() :
-    numbers = parse_numbers()
 
-    if numbers is not None:
-        result = analyze_numbers(numbers)
-        print_result(numbers, result) 
-    
-if __name__ == "__main__":
+
+def main():
+    while True:
+        numbers = parse_numbers()
+        if numbers == 'q':
+             print("程序结束")
+             break
+        if numbers is not None:
+            result = analyzer_numbers(numbers)
+            print_result(numbers,result)
+if __name__ == "__main__" :
     main()
-    
+
+
